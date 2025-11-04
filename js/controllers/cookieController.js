51f01becd568de5c329ff9837bc239b3e824d699
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-11-04 14:05:49 +0100

    started on cookie view

---
+++ js/controllers/cookieController.js
@@ -0,0 +1,19 @@
+import { CookieBannerView } from "../views/organisms/cookieView.js";
+
+export const CookieBanner = () =>{
+    const KEY = 'cookieConsent';
+    const getConsent = () => JSON.parse(localStorage.getItem(KEY) || null);
+    const setConsent = (c) => localStorage.setItem(KEY, JSON.stringify(c));
+    const saved = getConsent();
+
+    const banner = CookieBannerView();
+
+    const acceptAllBtn = banner.querySelector('#acceptAllCookiesBtn');
+    const acceptNecessaryBtn = banner.querySelector('#acceptNecessaryCookiesBtn');
+
+    if(!saved){
+        banner.style.display = 'block';
+    }
+
+    return banner;
+}
