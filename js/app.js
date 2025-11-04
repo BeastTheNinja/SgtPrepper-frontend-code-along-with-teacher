af44a371cb5128600f5d67ac62d3d246b608dd29
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-11-04 08:14:31 +0100

    added some seo and updated router

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 import { CartPage } from "./controllers/cartController.js";
+import { HomePage } from "./controllers/homeController.js";
 import { LoginPage } from "./controllers/loginController.js";
 import { ProductPage } from "./controllers/productController.js";
 import { router } from "./router/index.js";
 
 router(
   {
-    "/": () => ProductPage(), // Forside
+    // "/": () => HomePage(), // Forside
+    "/": () => ProductPage(), // Product Page
     "/login": () => LoginPage(), // Login Page
-    '/cart': () => CartPage(), // Cart Page
+    "/cart": () => CartPage(), // Cart Page
   },
   "#app"
 );
